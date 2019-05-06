d88b0d4882965201f89d54c6cfa12ae8d898d30c
Đoàn Mạnh Cường <36760823+manhcuongmas@users.noreply.github.com>
2019-05-06 21:08:32 +0700

    Add files via upload

---
+++ Cường/comment box/comment_box.php
@@ -0,0 +1,22 @@
+<li class="comment-holder" id="_1">
+<div class="user-img">
+<img src="avatar.jpg" class="user-img-pic" />
+</div>
+<div class="comment-body">
+<h3 class="username-field">
+Đoàn Mạnh Cường		
+</h3>
+<div class="comment-text">
+							
+</div>
+
+<div class="comment-button-holder">
+<ul>
+<li class="delete-btn">&#9776;</li>
+</ul>			
+</div>
+</div>
+						
+
+
+</li>
---
+++ Cường/comment box/define.php
@@ -0,0 +1,8 @@
+<?php 
+	
+	define('DOC_Root', $_SERVER['root'] . '/');
+	define('DS', DIRECTORY_SEPARATOR);
+	define('INCLUDES', DOC_Root . 'includes' . DS );
+
+
+ ?>
